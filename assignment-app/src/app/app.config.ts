efe58aa02ebc2b22f1534dc23d256228462ea824
tahasimse <tahabsgs@gmail.com>
2025-12-07 18:39:11 +0100

    Add build script to api/package.json and update date format

--- assignment-app/src/app/app.config.ts
+++ assignment-app/src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptorsFromDi()),
+    { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' }
   ],
 };
